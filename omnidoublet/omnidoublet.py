c28b2d36bd7b2196d64c66b3443f6321145bf0a7
LL <32667199+mmmads@users.noreply.github.com>
2025-06-14 12:51:00 +0800

    Update omnidoublet.py

--- omnidoublet/omnidoublet.py
+++ omnidoublet/omnidoublet.py
@@ -409,8 +409,9 @@ class OmniDoublet ():
         # normalization
         final_score = min_max_normalize(final_score)
 
-
         # threshold calcluation
+        real_scores = final_score[:self.num_cells]
+        sim_scores = final_score[self.num_cells:]
         threshold, _, _ = auto_semi_supervised_cutoff(real_scores, sim_scores, true_labels=init_labels, n_init=5, rseed=self.rseed)
         cls = (origin_pred >= threshold).astype(int)
 
